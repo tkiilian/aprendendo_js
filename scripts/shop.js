232341cf7e3949f159a30299c02f2ba2f56f2c80
tkiilian <thiedanny8@gmail.com>
2017-07-12 11:49:56 -0300

    aprendendo trabalhar com elementos HTML no JavaScript

---
+++ scripts/shop.js
@@ -0,0 +1,58 @@
+var total = document.getElementById("total");
+
+var formatedText = floatToMoneyText(moneyTextToFloat(total.innerText))
+
+
+// alert(readTotal());
+
+writeTotal(350.23);
+
+function moneyTextToFloat(text) {
+  var cleanText = text.replace("R$", "").replace(",", ".");
+
+  return parseFloat(cleanText);
+}
+
+// R$ 29,90 / 29,90 / 29.90
+
+// 2 == "2" true // 2 === "2" false
+
+function floatToMoneyText(value) {
+  var text = (value < 1 ? "0" : "") + Math.floor(value * 100);
+  text = "R$ " + text;
+
+  return text.substr(0, text.length - 2) + "," + text.substr(-2);
+}
+
+function readTotal() {
+  var total = document.getElementById("total");
+
+  return moneyTextToFloat(total.innerText);
+}
+
+function writeTotal(value) {
+  var total = document.getElementById("total");
+  total.innerHTML = floatToMoneyText(value);
+}
+
+
+
+function calcular() {
+
+  var produtos = document.getElementsByClassName("produto");
+  var soma = 0;
+
+
+  for (var i = 0; i < produtos.length; i++) {
+
+    var produto = produtos[i];
+
+    var price = produto.getElementsByClassName("price")[0];
+
+    var priceText = price.innerText;
+
+    soma = soma + moneyTextToFloat(priceText);
+  }
+
+  document.getElementById("total").innerText = soma;
+}
